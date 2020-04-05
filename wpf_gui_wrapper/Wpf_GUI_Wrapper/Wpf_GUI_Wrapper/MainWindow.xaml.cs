cb930e44329173e82516e10e0940e5ad4b78d0e6
Johnson Liu <johl@synaptics.com>
2020-04-06 00:00:12 +0800

    commit WPF wrapper for the console

--- wpf_gui_wrapper/Wpf_GUI_Wrapper/Wpf_GUI_Wrapper/MainWindow.xaml.cs
+++ wpf_gui_wrapper/Wpf_GUI_Wrapper/Wpf_GUI_Wrapper/MainWindow.xaml.cs
@@ -1,87 +1,52 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace winform_gui_wrapper
+namespace Wpf_GUI_Wrapper
 {
-    public partial class GUI : Form
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        public GUI()
+        public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            String ver = "0.1";
-            string msg = $"GUI Wrapper for Python Control:{ver}";
-            MessageBox.Show(msg, "About");
-
-        }
-
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabPage2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Browse_Click(object sender, EventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             string strPath = System.IO.Directory.GetCurrentDirectory();//取得当前默认路径
             //System.IO.Directory.SetCurrentDirectory(strPath);//还原默认路径
             openFileDialog1.InitialDirectory = strPath;    //打开对话框后的初始目录
             openFileDialog1.Filter = "文本文件|*.map|所有文件|*.*";
-            openFileDialog1.RestoreDirectory = false;    //若为false，则打开对话框后为上次的目录。若为true，则为初始目录
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            openFileDialog1.RestoreDirectory = false;    //若为false，则打开对话框后为上次的目录。若为true，则为初始目录
+            if (openFileDialog1.ShowDialog() == true)  
             {
+                //textBox1.Text = File.ReadAllText(openFileDialog.FileName);
                 textBox1.Text = System.IO.Path.GetFullPath(openFileDialog1.FileName);//将选中的文件的路径传递给TextBox “FilePath”
-                richTextBox1.Text = textBox1.Text;
+                richTextBox1.AppendText( textBox1.Text);
             }
             //create one thread to run the console and get back the result 
 
             //paste the result into the textbox 
         }
 
-
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Run_Click(object sender, EventArgs e)
+        private void Run_Click(object sender, RoutedEventArgs e)
         {
             RunPythonScript();
         }
@@ -121,7 +86,7 @@ namespace winform_gui_wrapper
                            sArguments += " " + args;
                */
                 sArguments += " ";
-                if(string.IsNullOrEmpty(textBox1.Text))
+                if (string.IsNullOrEmpty(textBox1.Text))
                     throw new System.InvalidOperationException("Input map file cannot be empty");
                 sArguments += textBox1.Text;
                 p.StartInfo.Arguments = sArguments;
@@ -148,10 +113,25 @@ namespace winform_gui_wrapper
                 string msg = $"There are something wrong:{e.Message}";
                 MessageBox.Show(msg, "ERROR");
             }
-            
-            
+
+
         }
 
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            Button_Click( sender,  e);
+        }
 
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            String ver = "0.1";
+            string msg = $"GUI Wrapper for Python Control:{ver}";
+            MessageBox.Show(msg, "About");
+        }
     }
 }
